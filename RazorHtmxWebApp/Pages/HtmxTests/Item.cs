9d4f005bad534c51ddef1b03ed4cd632ef6eef49
XmlNative <spalding.dan@usa.net>
2024-10-31 07:47:44 -0600

    Added Htmx Tests and About

---
+++ RazorHtmxWebApp/Pages/HtmxTests/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace RazorHtmxWebApp.Pages.HtmxTests
+{
+    public class Item
+    {
+        public Item()
+        {
+        }
+
+        public Item(string aName, string aDescription, Guid newGuid)
+        {
+            this.Name = aName;
+            this.Description = aDescription;
+            this.UniqueIdentifier = newGuid.ToString();
+        }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [DisplayName("ID")] public string? UniqueIdentifier { get; set; }
+
+    }
+}
+
